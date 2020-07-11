db8abff089c72bb62b8f0dd9b0e6dfae2b7a8d03
engrbugs <engrbugs@gmail.com>
2020-07-11 12:55:16 -0700

    2020-07-11-1255

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -50,93 +50,55 @@ const KEYS = [{
 },
 ];
 
-const bankTwo = [{
-keyCode: 81,
-keyTrigger: 'Q',
-id: 'Chord-1',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-}, {
-keyCode: 87,
-keyTrigger: 'W',
-id: 'Chord-2',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-}, {
-keyCode: 69,
-keyTrigger: 'E',
-id: 'Chord-3',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-}, {
-keyCode: 65,
-keyTrigger: 'A',
-id: 'Shaker',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-}, {
-keyCode: 83,
-keyTrigger: 'S',
-id: 'Open-HH',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-}, {
-keyCode: 68,
-keyTrigger: 'D',
-id: 'Closed-HH',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-}, {
-keyCode: 90,
-keyTrigger: 'Z',
-id: 'Punchy-Kick',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-}, {
-keyCode: 88,
-keyTrigger: 'X',
-id: 'Side-Stick',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-}, {
-keyCode: 67,
-keyTrigger: 'C',
-id: 'Snare',
-url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-}]
 
+class Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.playSound = this.playSound.bind(this);
+  }
+  playSound(e) {
+    const sound = document.getElementById(this.props.keyTrigger);
+    console.log(sound.currentSrc);
+    sound.currentTime = 0;
+    sound.play();
+  }
+  render () {
+    return  (
+      <div id={this.props.clipId}
+        onClick={this.playSound} 
+        className="box" 
+         >
+          <audio className='clip' id={this.props.keyTrigger} src={this.props.clip}></audio>
+          {this.props.keyTrigger}
+      </div>
 
+      // <div id={this.props.clipId} onClick={this.playSound}
+      // className="box">
+      //   <audio className='clip' id={this.props.keyTrigger} src={this.props.clip}></audio>
+      //   {this.props.keyTrigger}</div>
+    )
 
+    
+            // <div className="box" onClick={this.playSound}
+        // text={ key.keyTrigger } key={ idx }>
+        //   { key.keyTrigger }
+        // <audio ref={this.audio} id={key.keyTrigger}src={key.url}
+        // />
+        //   </div>
+  }
+}
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.playSound = this.playSound.bind(this);
-  this.audio = React.createRef();
-}
-playSound(e) {
-    console.log(this.audio.current)
-    this.audio.current.play();
-}
-  // playSound() {
-
-  //   const sound = document.getElementById(this.key.keyTrigger);
-  //   sound.currentTime = 0;
-  //   sound.play();
-  //   this.activatePad();
-  //   setTimeout(() => this.activatePad(), 100);
-  //   this.props.updateDisplay(this.props.clipId.replace(/-/g, ' '));
-
-  //   // console.log(this.audio.current)
-  //   // this.audio.current.play();
-  // }
 
   render() {
     return (
     <div id="display" className="App display">
-      
       {KEYS.map((key, idx)=>(
-        
-              <div id={key.clipId} 
-              onClick={this.playSound} 
-              className="box" 
-               >{key.keyTrigger}
-                <audio className='clip' id={key.keyTrigger} src={key.clip}></audio>
-                {/* {this.props.keyTrigger} */}
-            </div>
-
+        <Box
+          clipId={idx} 
+					clip={key.url}
+					keyTrigger={key.keyTrigger}
+					keyCode={key.keyCode} />
 
         // <div className="box" onClick={this.playSound}
         // text={ key.keyTrigger } key={ idx }>
