7f79d62b06d4c5fd882be36c6b694c388aeb07c6
engrbugs <engrbugs@gmail.com>
2020-07-10 20:21:28 -0700

    2020-07-10-2021

--- src/App.js
+++ src/App.js
@@ -2,25 +2,156 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const KEYS = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Heater-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Heater-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Heater-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Heater-4',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Clap',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: "Kick-n'-Hat",
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},
+];
+
+const bankTwo = [{
+keyCode: 81,
+keyTrigger: 'Q',
+id: 'Chord-1',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+keyCode: 87,
+keyTrigger: 'W',
+id: 'Chord-2',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+keyCode: 69,
+keyTrigger: 'E',
+id: 'Chord-3',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+keyCode: 65,
+keyTrigger: 'A',
+id: 'Shaker',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+}, {
+keyCode: 83,
+keyTrigger: 'S',
+id: 'Open-HH',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+keyCode: 68,
+keyTrigger: 'D',
+id: 'Closed-HH',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+keyCode: 90,
+keyTrigger: 'Z',
+id: 'Punchy-Kick',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+keyCode: 88,
+keyTrigger: 'X',
+id: 'Side-Stick',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+}, {
+keyCode: 67,
+keyTrigger: 'C',
+id: 'Snare',
+url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}]
+
+
+
+
+class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.playSound = this.playSound.bind(this);
+  this.audio = React.createRef();
 }
+playSound(e) {
+    console.log(this.audio.current)
+    this.audio.current.play();
+}
+  // playSound() {
+
+  //   const sound = document.getElementById(this.key.keyTrigger);
+  //   sound.currentTime = 0;
+  //   sound.play();
+  //   this.activatePad();
+  //   setTimeout(() => this.activatePad(), 100);
+  //   this.props.updateDisplay(this.props.clipId.replace(/-/g, ' '));
+
+  //   // console.log(this.audio.current)
+  //   // this.audio.current.play();
+  // }
+
+  render() {
+    return (
+    <div id="display" className="App display">
+      
+      {KEYS.map((key, idx)=>(
+        
+              <div id={key.clipId} 
+              onClick={this.playSound} 
+              className="box" 
+               >{key.keyTrigger}
+                <audio className='clip' id={key.keyTrigger} src={key.clip}></audio>
+                {/* {this.props.keyTrigger} */}
+            </div>
+
+
+        // <div className="box" onClick={this.playSound}
+        // text={ key.keyTrigger } key={ idx }>
+        //   { key.keyTrigger }
+        // <audio ref={this.audio} id={key.keyTrigger}src={key.url}
+        // />
+        //   </div>
+      ))}
+      
+    </div>
+    );
+  }
+}
+
+
+
 
 export default App;
