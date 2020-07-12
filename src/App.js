cd5a67b2620fe9da8e42cb07d0d875a503ead2e3
engrbugs <engrbugs@gmail.com>
2020-07-12 11:03:08 -0700

    2020-07-12-1103

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 const KEYS = [{
   keyCode: 81,
   keyTrigger: 'Q',
-  id: 'Heater-1',
+  id: 'Heat',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
 }, {
   keyCode: 87,
   keyTrigger: 'W',
-  id: 'Heater-2',
+  id: 'Sega',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
 }, {
   keyCode: 69,
   keyTrigger: 'E',
-  id: 'Heater-3',
+  id: 'Oscillation',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
 }, {
   keyCode: 65,
   keyTrigger: 'A',
-  id: 'Heater-4',
+  id: 'Guitar',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
 }, {
   keyCode: 83,
@@ -30,28 +30,28 @@ const KEYS = [{
 }, {
   keyCode: 68,
   keyTrigger: 'D',
-  id: 'Open-HH',
+  id: 'Snare',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
 }, {
   keyCode: 90,
   keyTrigger: 'Z',
-  id: "Kick-n'-Hat",
+  id: "Kick",
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
 }, {
   keyCode: 88,
   keyTrigger: 'X',
-  id: 'Kick',
+  id: 'Bass',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
 }, {
   keyCode: 67,
   keyTrigger: 'C',
-  id: 'Closed-HH',
+  id: 'Close',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
 },
 ];
 
 
-class Box extends React.Component {
+class BeatBox extends React.Component {
   constructor(props) {
     super(props);
     this.audio = React.createRef();
@@ -60,10 +60,10 @@ class Box extends React.Component {
   handleKeyPress = (e)=> {
     if (e.keyCode === this.props.keyCode) {
       const parent = document.getElementById(this.props.keyTrigger);
-      parent.parentNode.classList.toggle('box-active');
+      parent.parentNode.classList.toggle('drum-pad-active');
       console.log(parent.parentNode);
       this.playSound();
-      setTimeout(() => parent.parentNode.classList.toggle('box-active'), 100);
+      setTimeout(() => parent.parentNode.classList.toggle('drum-pad-active'), 100);
     }
   }
 
@@ -80,6 +80,7 @@ class Box extends React.Component {
   playSound = () => {
     const sound = document.getElementById(this.props.keyTrigger);
     console.log(sound.currentSrc);
+    this.props.updateDisplay(this.props.clipId);
     sound.currentTime = 0;
     sound.play();
   }
@@ -88,7 +89,7 @@ class Box extends React.Component {
       <div id={this.props.keyCode}
         onClick={this.playSound} 
         ref={this.audio}
-        className="box" 
+        className="drum-pad" 
          >
           <audio className='clip'  id={this.props.keyTrigger} src={this.props.clip}></audio>
           {this.props.keyTrigger}
@@ -111,20 +112,32 @@ class Box extends React.Component {
 }
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: String.fromCharCode(160),
+    }
+    this.displayClipName = this.displayClipName.bind(this);
+  }
+  displayClipName(name) {
+      this.setState({
+        display: name
+      });
+  }
   render() {
     return (
-    <div id="display" className="App display">
+    <div id="drum-machine" className="App display">
               <div className="logo">
             <div className="inner-logo">{'BeatBox'}<small>{' by angerBUGS'}</small></div>
           </div>
       {KEYS.map((key, idx)=>(
-        <Box
-          clipId={key.keyCode} 
+        <BeatBox
+          clipId={key.id} 
 					clip={key.url}
 					keyTrigger={key.keyTrigger}
-					keyCode={key.keyCode} />
-
+					keyCode={key.keyCode} 
+					updateDisplay={this.displayClipName} />
+         
         // <div className="box" onClick={this.playSound}
         // text={ key.keyTrigger } key={ idx }>
         //   { key.keyTrigger }
@@ -132,7 +145,9 @@ class App extends React.Component {
         // />
         //   </div>
       ))}
-      
+       <div id="display" className="display-text">
+          <div className="inner-text">{this.state.display}</div>
+        </div>
     </div>
     );
   }
